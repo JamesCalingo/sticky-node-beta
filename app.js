8b4b8dd47481c9f1282bc82cd77c7e8c09ef846c
JC <james.calingo@gmail.com>
2021-10-29 09:49:45 -0400

    updating html and some css

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const button = document.querySelector("button")
 const board = document.querySelector(".board")
+let idNumber = 1
 
 function createNewNote (event) {
   event.preventDefault()
+  let notes = document.querySelectorAll("textarea")
+  console.log(notes)
  const note =  document.createElement("textarea")
  note.classList.add("sticky")
+ note.id = "note" + idNumber
  note.placeholder = "Write your note here"
  board.appendChild(note)
+ idNumber++
 }
 
 button.addEventListener("click", createNewNote)
